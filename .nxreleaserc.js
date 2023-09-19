d903f81c84ab1feb5bf60bf269de42a63553b6bf
Yevhen Zhylin <eugene.zhylin@mckesson.com>
2023-09-19 10:42:30 +0300

    feat(host): fancy text

--- .nxreleaserc.js
+++ .nxreleaserc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  "changelog": true,
-  "npm": false,
-  "github": true,
-  "repositoryUrl": "https://github.com/yzhylin/react-module-federation.git",
-  "branches": [
+  changelog: true,
+  npm: false,
+  github: true,
+  repositoryUrl: "https://github.com/yzhylin/react-module-federation.git",
+  branches: [
     "main"
   ],
   tagFormat: "${PROJECT_NAME}/v${VERSION}",
   commitMessage: "chore(release): 🚀 release ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
-  "plugins": [
+  plugins: [
     "semantic-release-unsquash"
   ]
 }
