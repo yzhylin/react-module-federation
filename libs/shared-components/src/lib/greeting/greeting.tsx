9005ddc728e65e8af4460b94bf8280f234b6b852
Yevhen Zhylin <eugene.zhylin@mckesson.com>
2023-10-08 23:29:40 +0300

    feat(plugin): check affected

--- libs/shared-components/src/lib/greeting/greeting.tsx
+++ libs/shared-components/src/lib/greeting/greeting.tsx
@@ -779,7 +779,7 @@ export function Greeting({ title }: GreetingProps) {
                     d="M8 9l3 3-3 3m5 0h3M5 20h14a2 2 0 002-2V6a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
                   />
                 </svg>
-                Run affected commands
+                Run affected commands!
               </summary>
               <pre>
                 <span># see what&apos;s been affected by changes</span>
